412dcd15c108f52dffed150cc5d4c0f60060ee15
tominator633 <t-ruz@seznam.cz>
2024-08-18 09:57:49 +0200

    reddit detail window and comments design enhance

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -29,7 +29,7 @@ export default function Comment ({content}) {
             
                 {content.replies.length>0 && <button onClick={handleRepliesButtonClick} 
                                                     className={`${styles.repliesButton} ${styles.gb}`}
-                                                    style={repliesButton ? {backgroundColor: "#FF6B6B", color: "#005792"}:{backgroundColor: "#005792", color: "white"}}>
+                                                    style={repliesButton ? {backgroundColor: "#FF6B6B", color: "white"}:{backgroundColor: "#005792", color: "white"}}>
                                                         <span>Replies </span>
                                                         <span>{`(${content.replies.length}) `}</span>
                                                         {repliesButton && <span className={`${styles.closeCross} ${styles.gb}`}><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024"><path fill="currentColor" d="M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504L738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512L828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496L285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512L195.2 285.696a64 64 0 0 1 0-90.496"/></svg></span>}
--- src/components/RedditDetailWindow/RedditDetailWindow.js
+++ src/components/RedditDetailWindow/RedditDetailWindow.js
@@ -29,29 +29,26 @@ export default function RedditDetailWindow () {
     return (
 <div id={redditId} className={`${styles.windowBarrier} ${styles.gb}`} role="presentation">
     <section className={`${styles.redditDetailWindow} ${styles.gb}`} role="dialog" aria-label="reddit detail window">
-        <div className={`${styles.closeBtnCon} ${styles.gb}`} role="presentation">
-            <button onClick={handleCloseButtonClick} className={`${styles.closeBtn} ${styles.gb}`}>Close</button>
-        </div>
+        <button onClick={handleCloseButtonClick} className={`${styles.closeBtn} ${styles.gb} ${styles.clearfix}`}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024"><path fill="currentColor" d="M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504L738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512L828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496L285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512L195.2 285.696a64 64 0 0 1 0-90.496"/></svg>
+        </button>
         <div className={styles.redditDetail} >
-                <div className={`${styles.votesColumn} ${styles.gb}`} role="presentation">
-                    <button className={`${styles.arrowUp} ${styles.gb}`} aria-label="upvote this reddit">
+            <div className={`${styles.redditInfoLine} ${styles.gb}`} role="presentation">
+                <div className={`${styles.scoreDiv} ${styles.gb}`}>
+                    <figure className={`${styles.arrowUp} ${styles.gb}`}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M10.586 3L4 9.586a2 2 0 0 0-.434 2.18l.068.145A2 2 0 0 0 5.414 13H8v7a2 2 0 0 0 2 2h4l.15-.005A2 2 0 0 0 16 20l-.001-7h2.587A2 2 0 0 0 20 9.586L13.414 3a2 2 0 0 0-2.828 0"></path></svg>
-                    </button>
+                    </figure>
                     <p className={`${styles.votes} ${styles.gb}`} aria-label={`The score of this reddit is ${currentReddit.score}`}>{currentReddit.score}</p>
-                    <button className={`${styles.arrowDown} ${styles.gb}`}  aria-label="downvote this reddit">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m10 2l-.15.005A2 2 0 0 0 8 4v6.999L5.414 11A2 2 0 0 0 4 14.414L10.586 21a2 2 0 0 0 2.828 0L20 14.414a2 2 0 0 0 .434-2.18l-.068-.145A2 2 0 0 0 18.586 11L16 10.999V4a2 2 0 0 0-2-2z"></path></svg>
-                    </button>
                 </div>
-                <div className={`${styles.redditColumn} ${styles.gb}`} role="presentation">
-                    <div className={`${styles.redditInfo} ${styles.gb}`} role="presentation">
-                        <p className={`${styles.redditUser} ${styles.gb}`}>{currentReddit.user}</p>
-                        <p className={`${styles.redditTimePosted} ${styles.gb}`}>{epochToAgo(currentReddit.created)}</p>
-                    </div>
-                    <div className={`${styles.redditContent} ${styles.gb}`}>
-                        <h4>{currentReddit.title}</h4>
-                    </div>
-                </div>
+                <p className={`${styles.redditUser} ${styles.gb}`}>{currentReddit.user}</p>
+                <p className={`${styles.redditTimePosted} ${styles.gb}`}>{epochToAgo(currentReddit.created)}</p>
+
             </div>
+            <div className={`${styles.redditTitle} ${styles.gb}`} role="presentation">
+                <h4>{currentReddit.title}</h4>
+            </div>
+        </div>
+        <h2 className={`${styles.commentsHeadline} ${styles.gb}`}>{`Comments (${comments.length})`}</h2>
         <div className={`${styles.commentsSection} ${styles.gb}`}>
             {isCommentsLoading ? 
             <Loading loadingText="Loading comments..."/> 
@@ -60,7 +57,7 @@ export default function RedditDetailWindow () {
             <ErrorMessage/>
             :
             comments.length === 0 ?
-            <p>This post has no comments</p>
+            <p className={`${styles.noComments} ${styles.gb}`}>This post has no comments</p>
             :
             comments.map((content, index) => {
                     return <Comment content={content} key={index}/>
